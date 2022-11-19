4de32afae1a1c373c35dd8bdcaf366afa0f9bdd2
ekRoberts <113919775+ekRoberts@users.noreply.github.com>
2022-11-19 14:47:08 -0700

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Header(props) {
+
+    return (
+            <div className = "header">
+            Make a Dungeons and Dragons Party
+                  <hr/>
+            </div>
+        );
+  }
+
+export default Header;
