ca700afdfa6645ff3b8d1ee75ae488ccf5ef9a2f
ekRoberts <113919775+ekRoberts@users.noreply.github.com>
2022-11-19 14:49:40 -0700

    Create classes.js

---
+++ classes.js
@@ -0,0 +1,89 @@
+const classes = [
+  {
+    charclass: 'Barbarian',
+    hitDie: 'd12',
+    primaryAbility: 'Strength',
+    secondaryAbility: 'Constitution',
+    armor: 'Light or Medium',
+  },
+  {
+    charclass: 'Bard',
+    hitDie: 'd8',
+    primaryAbility: 'Charisma',
+    secondaryAbility: 'Dexterity or Strength',
+    armor: 'Light',
+  },
+  {
+    charclass: 'Cleric',
+    hitDie: 'd8',
+    primaryAbility: 'Wisdom',
+    secondaryAbility: 'Dexterity or Strength',
+    armor: 'Light or Medium',
+  },
+  {
+    charclass: 'Druid',
+    hitDie: 'd8',
+    primaryAbility: 'Wisdom',
+    secondaryAbility: 'Constitution',
+    armor: 'Light or Medium',
+  },
+  {
+    charclass: 'Fighter',
+    hitDie: 'd10',
+    primaryAbility: 'Dexterity or Strength',
+    secondaryAbility: 'Constitution',
+    armor: 'Light, Medium, or Heavy',
+  },
+  {
+    charclass: 'Monk',
+    hitDie: 'd8',
+    primaryAbility: 'Dexterity',
+    secondaryAbility: 'Wisdom',
+    armor: 'None',
+  },
+  {
+    charclass: 'Paladin',
+    hitDie: 'd10',
+    primaryAbility: 'Strength',
+    secondaryAbility: 'Charisma',
+    armor: 'Light, Medium, or Heavy',
+  },
+  {
+    charclass: 'Ranger',
+    hitDie: 'd10',
+    primaryAbility: 'Dexterity',
+    secondaryAbility: 'Wisdom',
+    armor: 'Light or Medium',
+  },
+  {
+    charclass: 'Rogue',
+    hitDie: 'd8',
+    primaryAbility: 'Dexterity',
+    secondaryAbility: 'None',
+    armor: 'Light',
+  },
+  {
+    charclass: 'Sorcerer',
+    hitDie: 'd6',
+    primaryAbility: 'Charisma',
+    secondaryAbility: 'Constitution',
+    armor: 'None',
+  },
+  {
+    charclass: 'Warlock',
+    hitDie: 'd8',
+    primaryAbility: 'Charisma',
+    secondaryAbility: 'Constitution',
+    armor: 'Light',
+  },
+  {
+    charclass: 'Wizard',
+    hitDie: 'd6',
+    primaryAbility: 'Intelligence',
+    secondaryAbility: 'Constitution',
+    armor: 'None',
+  },
+
+];
+
+module.exports = classes;
