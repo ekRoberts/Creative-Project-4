4986bec075399dac984226a62712b084373c9a18
ekRoberts <113919775+ekRoberts@users.noreply.github.com>
2022-11-19 14:50:05 -0700

    Create races.js

---
+++ races.js
@@ -0,0 +1,59 @@
+const races = [
+  {
+    race: 'Dwarf',
+    size: 'Medium',
+    speed: '25',
+    lang: 'Dwarvish',
+  },
+  {
+    race: 'Elf',
+    size: 'Medium',
+    speed: '30',
+    lang: 'Elvish',
+  },
+  {
+    race: 'Halfing',
+    size: 'Small',
+    speed: '25',
+    lang: 'Halfling',
+  },
+  {
+    race: 'Human',
+    size: 'Medium',
+    speed: '30',
+    lang: 'Language of Choice',
+  },
+  {
+    race: 'Dragonborn',
+    size: 'Medium',
+    speed: '30',
+    lang: 'Draconic',
+  },
+  {
+    race: 'Gnome',
+    size: 'Small',
+    speed: '25',
+    lang: 'Gnomish',
+  },
+  {
+    race: 'Half-Elf',
+    size: 'Medium',
+    speed: '30',
+    lang: 'Elvish and Language of Choice',
+  },
+  {
+    race: 'Half-Orc',
+    size: 'Medium',
+    speed: '30',
+    lang: 'Goblin',
+  },
+  {
+    race: 'Tiefling',
+    size: 'Medium',
+    speed: '30',
+    lang: 'Infernal',
+  },
+
+];
+
+module.exports = races;
