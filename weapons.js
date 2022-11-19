9ce38d830ed9eadd9a901ef0ee0c46e24657059e
ekRoberts <113919775+ekRoberts@users.noreply.github.com>
2022-11-19 14:50:46 -0700

    Create weapons.js

---
+++ weapons.js
@@ -0,0 +1,69 @@
+const weapons = [
+  {
+    weapon: 'Club',
+    damage: '1d4 bludgeoning',
+    cost: '1 sp',
+  },
+  {
+    weapon: 'Dagger',
+    damage: '1d4 piercing',
+    cost: '2 gp',
+  },
+  {
+    weapon: 'Handaxe',
+    damage: '1d6 slashing',
+    cost: '5 gp',
+  },
+  {
+    weapon: 'Javelin',
+    damage: '1d6 piercing',
+    cost: '5 sp',
+  },
+  {
+    weapon: 'Quarterstaff',
+    damage: '1d6 bludgeoning',
+    cost: '2 sp',
+  },
+  {
+    weapon: 'Crossbow, light',
+    damage: '1d8 piercing',
+    cost: '25 gp',
+  },
+  {
+    weapon: 'Shortbow',
+    damage: '1d6 piercing',
+    cost: '25 gp',
+  },
+  {
+    weapon: 'Battleaxe',
+    damage: '1d8 slashing',
+    cost: '10 gp',
+  },
+  {
+    weapon: 'Lance',
+    damage: '1d12 piercing',
+    cost: '10 gp',
+  },
+  {
+    weapon: 'Longsword',
+    damage: '1d8 slashing',
+    cost: '15 gp',
+  },
+  {
+    weapon: 'Shortsword',
+    damage: '1d6 piercing',
+    cost: '10 gp',
+  },
+  {
+    weapon: 'Trident',
+    damage: '1d6 piercing',
+    cost: '5 gp',
+  },
+  {
+    weapon: 'Longbow',
+    damage: '1d8 piercing',
+    cost: '50 gp',
+  },  
+];
+
+module.exports = weapons;
